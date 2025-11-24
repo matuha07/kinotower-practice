78cd7bd5e61da4e9911d77b6e90173a6b7410fcf
motya <mmotyaa@proton.me>
2025-11-24 14:00:44 +0500

    add factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Gender;
+use App\Models\Country;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +21,46 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+
+        Gender::factory()->create([ 'name' => 'Male']);
+        Gender::factory()->create([ 'name' => 'Female']);
+
+        Country::factory()->create([ 'name' => 'United States']);
+        Country::factory()->create([ 'name' => 'Israel']);
+        Country::factory()->create([ 'name' => 'Japan']);
+        Country::factory()->create([ 'name' => 'Kazakhstan']);
+        Country::factory()->create([ 'name' => 'China']);
+        Country::factory()->create([ 'name' => 'Ukraine']);
+
+
+        Category::factory()->create([ 'name' => 'Anime']);
+        Category::factory()->create([ 'name' => 'Action']);
+        Category::factory()->create([ 'name' => 'Adventure']);
+        Category::factory()->create([ 'name' => 'Thriller']);
+        Category::factory()->create([ 'name' => 'Science']);
+        Category::factory()->create([ 'name' => 'Comedy']);
+        Category::factory()->create([ 'name' => 'Sport']);
+        Category::factory()->create([ 'name' => 'Drama']);
+        Category::factory()->create([ 'name' => 'Horror']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
